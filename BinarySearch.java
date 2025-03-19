7aaf2b1dc59b567efc97c6b2862ec51a7ac2d95a
Suriya <suriyastart007@gmail.com>
2025-03-19 22:57:31 +0530

    Binary search in progress

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class BinarySearch {
@@ -6,12 +5,12 @@ public class BinarySearch {
         int lowerBound = -1;
         int low = 0;
         int mid = 0;
-        int high = nums.length;
+        int high = nums.length-1;
         int upperBound = -1;
         Arrays.sort(nums);
         while (low <= high) {
             mid = (low + high) / 2;
-            if (nums[mid] < x) {
+            if (nums[mid] >= x) {
                 lowerBound = nums[mid]; // Potential floor
                 low = mid + 1; // Move right to find a larger value
             } else {
@@ -25,16 +24,16 @@ public class BinarySearch {
     public static int findLowerBound(int[] nums, int x) {
         int lowerBound = -1;
         int low = 0;
-        int high = nums.length;
+        int high = nums.length - 1;
         int mid = 0;
         Arrays.sort(nums);
         while (low <= high) {
             mid = (low + high) / 2;
             if (x <= nums[mid]) {
                 lowerBound = nums[mid];
-                low = mid + 1;
-            } else {
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return lowerBound;
@@ -43,7 +42,7 @@ public class BinarySearch {
     public static int findUpperBound(int[] nums, int x) {
         int upperBound = -1;
         int low = 0;
-        int high = nums.length;
+        int high = nums.length - 1;
         int mid = 0;
         Arrays.sort(nums);
         while (low <= high) {
@@ -58,11 +57,28 @@ public class BinarySearch {
         return upperBound;
     }
 
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        int ans = -1;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (target <= nums[mid]) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
-        int[] nums = { 5, 6, 8, 9, 6, 5, 5, 6 };
+        int[] nums = { 1,3,5,6 };
         // System.out.println(Arrays.toString(findLowerBoundAndUpperBound(nums, 7)));
-        System.out.println(findLowerBound(nums, 7));
+        System.out.println(searchInsert(nums, 0));
 
     }
 }
