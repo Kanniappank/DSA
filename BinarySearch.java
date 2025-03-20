1c3ee85a427de61430989d63e7a8451f8fdcdb43
Suriya <suriyastart007@gmail.com>
2025-03-20 23:06:39 +0530

    BS on sorted rotated with duplicate elements

--- BinarySearch.java
+++ BinarySearch.java
@@ -115,13 +115,49 @@ public class BinarySearch {
         return -1;
     }
 
+    public static boolean findTargetSortedRotatedArrWithDuplicates(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (target == nums[mid]) {
+                return true;
+            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low += 1;
+                high -= 1;
+            } else {
+                if (nums[low] <= nums[mid]) {
+                    if (nums[low] <= target && target <= nums[mid]) {
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+
+                } else {
+                    if (nums[mid] <= target && target <= nums[high]) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
-        int[] nums = { 1, 3, 5, 6 };
+        // int[] nums = { 1, 3, 5, 6 };
         // System.out.println(Arrays.toString(findLowerBoundAndUpperBound(nums, 7)));
-        System.out.println(searchInsert(nums, 0));
-        int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
-        System.out.println(findTargetSortedRotatedArr(nums, 0));
+        // System.out.println(searchInsert(nums, 0));
+        // int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+        // System.out.println(findTargetSortedRotatedArr(nums, 0));
+        int[] nums={2,5,6,0,0,1,2};
+        System.out.println(findTargetSortedRotatedArrWithDuplicates(nums, 0));
 
     }
 }
