8403a1f56e537e9d3db566cf3dfad80a25ca4daa
Suriya <suriyastart007@gmail.com>
2025-03-24 22:31:08 +0530

    WIP BS

--- BinarySearch.java
+++ BinarySearch.java
@@ -196,14 +196,6 @@ public class BinarySearch {
         return -1;
     }
 
-    public static int[] firstAndLastOccurenceSeperate(int[] nums, int target) {
-        int first = firstOccurence(nums, target);
-        if (first == -1) {
-            return new int[] { -1, -1 };
-        }
-        return new int[] { firstOccurence(nums, target), lastOccurance(nums, target) };
-    }
-
     public static boolean findTargetSortedRotatedArrWithDuplicates(int[] nums, int target) {
 
         int low = 0;
@@ -238,6 +230,110 @@ public class BinarySearch {
         return false;
     }
 
+    public static int[] firstAndLastOccurenceSeperate(int[] nums, int target) {
+        int first = firstOccurence(nums, target);
+        if (first == -1) {
+            return new int[] { -1, -1 };
+        }
+        return new int[] { firstOccurence(nums, target), lastOccurance(nums, target) };
+    }
+
+    public static int minimumOfSortedRotatedArr(int[] nums) {
+        /*
+         * Initialize ans = Integer.MAX_VALUE to track the minimum value.
+         * 
+         * Binary Search (low <= high):
+         * 
+         * If left half (nums[low] to nums[mid]) is sorted, update ans with nums[low]
+         * and move right (low = mid + 1).
+         * 
+         * Otherwise, right half is sorted, update ans with nums[mid] and move left
+         * (high = mid).
+         * 
+         * Return ans after the loop.
+         * 
+         * Time Complexity
+         * O(log n) (Binary Search)
+         */
+
+        int ans = Integer.MAX_VALUE;
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[low] <= nums[mid]) {
+                ans = Math.min(ans, nums[low]);
+                low = mid + 1;
+            } else {
+                ans = Math.min(ans, nums[mid]);
+                high = mid;
+            }
+
+        }
+        return ans;
+    }
+
+    // public static int noOfRotationsUsingBinarySearch(int[] nums) {
+
+    // }
+
+    public static int singleElementInSortedArray(int[] nums) {
+        /*
+         * Brute force
+         * search linearly and add the conditions
+         */
+        // if (nums.length == 1) {
+        // return nums[0];
+        // }
+        // for (int i = 0; i < nums.length; i++) {
+        // if (i == 0) {
+        // if (nums[i] != nums[i + 1]) {
+        // return nums[i];
+        // }
+        // } else if (i == nums[nums.length - 1]) {
+        // if (nums[i] != nums[i - 1]) {
+        // return nums[i];
+        // }
+        // } else {
+        // if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+        // return nums[i];
+        // }
+        // }
+        // }
+        // return -1;
+
+        /*
+         * Optimal Solution
+         * 
+         */
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[len - 2] != nums[len - 1]) {
+            return nums[len - 1];
+        }
+        int low = 1;
+        int high = len - 2;
+        int mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            }
+            if (mid % 2 == 1 && nums[mid - 1] == nums[mid] || mid % 2 == 0 && nums[mid] == nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
 
         // int[] nums = { 3, 4, 4, 7, 8, 10 };
@@ -253,8 +349,12 @@ public class BinarySearch {
         // int[] nums = { 2, 5, 6, 0, 0, 1, 2 };
         // System.out.println(findTargetSortedRotatedArrWithDuplicates(nums, 0));
 
-        int[] nums = { 1, 1, 2, 2, 2, 2, 3 };
-        System.out.println(Arrays.toString(firstAndLastOccurenceSeperate(nums, 2)));
+        // int[] nums = { 1, 1, 2, 2, 2, 2, 3 };
+        // System.out.println(Arrays.toString(firstAndLastOccurenceSeperate(nums, 2)));
+        // int[] nums = { 39, 6, 11, 14, 18, 36, 37, 38 };
+        // System.out.println(noOfRotationsUsingBinarySearch(nums));
+        int[] nums={1,1,2,3,3,4,4,8,8};
+        System.out.println(singleElementInSortedArray(nums));
 
     }
 }
