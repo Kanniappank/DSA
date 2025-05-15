35bb109b487be49a43cd9160d8546be94d2259cb
Kanniappan K <kkumaresan@thapovan-inc.com>
2025-05-15 18:31:27 +0530

    WIP

--- Hashing.java
+++ Hashing.java
@@ -53,26 +53,68 @@ public class Hashing {
         }
         return -1;
     }
-    public static List repeatingNumberOfNby3times(int[] arr){
+
+    public static List repeatingNumberOfNby3times(int[] arr) {
         int len = arr.length;
-        int requiredCount = len/3;
-        Map<Integer,Integer> hash = new HashMap<>();
+        int requiredCount = len / 3;
+        Map<Integer, Integer> hash = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
-        for(int i = 0;i<len;i++){
-            hash.put(arr[i],hash.getOrDefault(arr[i], 0)+1);
+        for (int i = 0; i < len; i++) {
+            hash.put(arr[i], hash.getOrDefault(arr[i], 0) + 1);
         }
         System.out.println(hash);
         System.out.println(requiredCount);
-        for(Map.Entry<Integer,Integer> entry:hash.entrySet()){
-            if(entry.getValue()>requiredCount){
+        for (Map.Entry<Integer, Integer> entry : hash.entrySet()) {
+            if (entry.getValue() > requiredCount) {
                 ans.add(entry.getKey());
             }
         }
         return ans;
     }
 
+    public static int longestSubarraywithSum0(int[] arr) {
+        int len = arr.length;
+        int sum = 0;
+        int maxLength = 0;
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            sum += arr[i];
+            if (sum == 0) {
+                maxLength = i + 1;
+            }
+            if (hash.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - hash.get(sum));
+            } else {
+                hash.put(sum, i);
+            }
+        }
+        return maxLength;
+    }
+
+    public static int longestSubarrayWithTargetSum(int[] arr, int target) {
+        int len = arr.length;
+        int sum = 0;
+        int maxLength=0;
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            sum += arr[i];
+            if(sum == target){
+                maxLength=i+1;
+            }
+            int requiredSum=sum-target;
+            if(hash.containsKey(requiredSum)){
+                maxLength=Math.max(maxLength,i-hash.get(target));
+            }
+            // else{
+                hash.put(sum,i);
+            // }
+            System.out.println(hash);
+        }
+        return maxLength;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3 };
-        System.out.println(repeatingNumberOfNby3times(arr));
+        int[] arr = { 3, 4, -7, 1, 3, 3, 1, -4 };
+        System.out.println(longestSubarrayWithTargetSum(arr,7));
     }
 }
