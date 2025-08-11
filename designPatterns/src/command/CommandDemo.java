cbf7fb67e6324d1b22c541dc7d4f036ce1ac2d69
Kanniappan <kkumaresan@thapovan-inc.com>
2025-08-11 14:30:42 +0530

    command pattern complete

--- designPatterns/src/command/CommandDemo.java
+++ designPatterns/src/command/CommandDemo.java
@@ -3,6 +3,17 @@ package command;
 public class CommandDemo {
     public static void main(String[] args) {
 
+        AirConditioner AC = new AirConditioner();
+        TurnOnACCommand turnOnACCommand = new TurnOnACCommand(AC);
+        TurnOffACCommand turnOffACCommand = new TurnOffACCommand(AC);
+        RemoteControl remote = new RemoteControl();
+        remote.setCommand(turnOnACCommand);
+        remote.pressButton();
+        remote.undoButton();
+
+        remote.setCommand(turnOffACCommand);
+        remote.undoButton();
+        remote.pressButton();
 
     }
 }
---
+++ designPatterns/src/command/RemoteControl.java
@@ -0,0 +1,17 @@
+package command;
+
+public class RemoteControl {
+    private ICommand command;
+
+    public void setCommand(ICommand command) {
+        this.command = command;
+    }
+
+    public void pressButton(){
+        command.execute();
+    }
+
+    public void undoButton(){
+        command.undo();
+    }
+}
